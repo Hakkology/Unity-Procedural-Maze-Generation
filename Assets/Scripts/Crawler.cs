d446593bcd2ea9a9452565032892607b8a834eb4
Hakkology <yildizhakan88@gmail.com>
2025-08-15 11:52:03 +0300

    crawler fix, removed oop dependency

--- Assets/Scripts/Crawler.cs
+++ Assets/Scripts/Crawler.cs
@@ -6,38 +6,27 @@ public class Crawler : Maze
 {
     public override void Generate()
     {
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < 4; i++)
             CrawlV();
 
         for (int i = 0; i < 3; i++)
             CrawlH();
     }
 
-    void SetCorridor(int x, int z)
-    {
-        if (map[x, z] == 1)
-        {
-            map[x, z] = 0;
-            corridorLocations.Add(new MapLocation(x, z));
-            wallLocations.RemoveAll(loc => loc.x == x && loc.z == z);
-        }
-    }
-
     void CrawlV()
     {
         bool done = false;
-        int x = Random.Range(1, width - 1);
+        int x = Random.Range(1,width-1);
         int z = 1;
 
         while (!done)
         {
-            SetCorridor(x, z);
+            map[x, z] = 0;
             if (Random.Range(0, 100) < 50)
                 x += Random.Range(-1, 2);
             else
                 z += Random.Range(0, 2);
-
-            done |= (x < 1 || x >= width - 1 || z < 1 || z >= depth - 1);
+            done |= (x < 1 || x >= width-1 || z < 1 || z >= depth-1);
         }
     }
 
@@ -45,17 +34,16 @@ public class Crawler : Maze
     {
         bool done = false;
         int x = 1;
-        int z = Random.Range(1, depth - 1);
+        int z = Random.Range(1,depth-1);
 
         while (!done)
         {
-            SetCorridor(x, z);
+            map[x, z] = 0;
             if (Random.Range(0, 100) < 50)
                 x += Random.Range(0, 2);
             else
                 z += Random.Range(-1, 2);
-
-            done |= (x < 1 || x >= width - 1 || z < 1 || z >= depth - 1);
+            done |= (x < 1 || x >= width-2 || z < 1 || z >= depth-2);
         }
     }
 }
